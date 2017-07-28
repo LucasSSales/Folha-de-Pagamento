2315c34cf4f320af72b0511554667fed21519e20
LucasSSales <ldss@ic.ufal.br>
2017-07-28 10:58:27 -0300

    Update Assalariados.java

--- Assalariados.java
+++ Assalariados.java
@@ -2,4 +2,14 @@ package folhaDePagamento;
 
 public class Assalariados extends Empregado {
 	
+	public Assalariados() {
+		super();
+		setTipo(2);
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString() + "\nTIPO: Assalariado\n";
+	}
 }
